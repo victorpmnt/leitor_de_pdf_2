a159cddf4cc76ca1f319a41c85e41c6c92d60eaa
Victor Pimenta <victorpimenta9@gmail.com>
2024-08-30 14:58:41 -0300

    primeiro commit

---
+++ piton.py
@@ -0,0 +1,10 @@
+def welcome_message(name):
+    print(f"Bem-vindo, {name}!")
+
+def sum_two_numbers(a, b):
+    return a + b
+
+# Testando as funções
+welcome_message("Usuário")
+resultado = sum_two_numbers(5, 3)
+print(f"A soma de 5 e 3 é: {resultado}")
